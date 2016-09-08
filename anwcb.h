5d556396a1ecad83c628fcc925eb28c66705b6f6
Elliott Sobek <elliottsobek@gmail.com>
2016-09-07 22:23:13 -0600

    Create initial UI & logic

---
+++ anwcb.h
@@ -0,0 +1,51 @@
+/*
+* Alberta Net Wage Calculator (Basic)
+* Copyright (C) 2016  Author: Elliott Sobek
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*/
+
+#include <stdbool.h>
+
+/* Guard */
+#ifndef ANWCB_H
+#define ANWCB_H
+
+/* Macors */
+#define EI 0.0188
+#define CPP 0.0495
+
+/* Globals */
+short int hoursWorked;
+short int overtimeHours;
+double payRate;
+
+// Prototypes //
+
+/* anwcb prototypes */
+int main(void);
+void initialize(void);
+
+/* mainMenu prototypes */
+void mainMenu(void);
+void setPayRate(void);
+void setAmountOfHours(void);
+void setOverTimeHours(void);
+short int getMainMenuInput(void);
+double calculateNetIncome(void);
+double calculateGrossIncome(void);
+double calculateDeductions(double);
+
+/* shared prototypes */
+void purgeBuffer(void);
+bool isValidBounds(double, short int, int);
+
+#endif // !ANWCB_H
