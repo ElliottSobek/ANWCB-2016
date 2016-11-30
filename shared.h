e78420411dcaa69e251ddf36cd82b36e2f0786c3
Elliott Sobek <elliottsobek@gmail.com>
2016-11-30 11:15:43 -0700

    Add headers for appropriate files

---
+++ shared.h
@@ -0,0 +1,22 @@
+/*
+ * Alberta Net Wage Calculator (Basic)
+ * Copyright (C) 2016  Author: Elliott Sobek
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef SHARED_H
+#define SHARED_H
+
+void purgeBuffer(void);
+bool isValidBounds(double, short, int);
+
+#endif /* End SHARED_H */
